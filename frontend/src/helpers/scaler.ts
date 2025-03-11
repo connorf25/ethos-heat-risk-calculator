3aacba43952d113aedd8c6b878ee8262e83a9e1b
Connor Forbes <connorforbes25@gmail.com>
2025-03-11 15:58:42 +1000

    fix: Normalize values before running ridge regression

--- frontend/src/helpers/scaler.ts
+++ frontend/src/helpers/scaler.ts
@@ -1,12 +1,19 @@
 // scaler.ts
 
 interface MinMaxScalerParams {
-  min: number[] // the min_ attribute taken from sklearn.preprocessing.MinMaxScaler
-  scale: number[] // the scale_ attribute taken from sklearn.preprocessing.MinMaxScaler
+  min: readonly [...number[]] // the min_ attribute taken from sklearn.preprocessing.MinMaxScaler
+  scale: readonly [...number[]] // the scale_ attribute taken from sklearn.preprocessing.MinMaxScaler
 }
 
-// TODO: Placeholder - Replace with values from final trained model
-export const featuresScalerParams: MinMaxScalerParams = {
+// Helper type to create a tuple of specific length
+type NumberTuple<T extends readonly number[], N extends number = T['length']> = [
+  number,
+  ...number[],
+] & { length: N }
+
+// Scaler parameters
+// TODO: Placeholder values - replace with values trained on whole dataset
+export const featuresScalerParams = {
   min: [
     0.0, -0.3114754098360656, -3.020833333333333, -0.6183587248751761, -1.222222222222222,
     -0.21951219512195122, -11.197107405358395, -2.105553500973682,
@@ -15,38 +22,51 @@ export const featuresScalerParams: MinMaxScalerParams = {
     1.0, 0.01639344262295082, 0.020833333333333332, 0.012802458071949815, 0.05555555555555555,
     0.024390243902439025, 0.31613056192207334, 0.08119094120192633,
   ],
-}
-
-// TODO: Placeholder - Replace with values from final trained model
-export const outputScalerParams: MinMaxScalerParams = {
+} as const
+// TODO: Placeholder values - replace with values trained on whole dataset
+export const outputScalerParams = {
   min: [-11.197107405358395, -2.0197777680408033],
   scale: [0.31613056192207334, 0.07894843838015173],
-}
+} as const
 
-export class MinMaxScaler {
-  private min: number[]
-  private scale: number[]
+export class MinMaxScaler<N extends number> {
+  private min: readonly number[]
+  private scale: readonly number[]
+  private length: N
 
   constructor(params: MinMaxScalerParams) {
+    if (params.min.length !== params.scale.length) {
+      throw new Error('Min and scale arrays must have the same length')
+    }
+
     this.min = params.min
     this.scale = params.scale
+    this.length = params.min.length as N
   }
 
-  transform(values: number[]): number[] {
+  transform<A extends NumberTuple<typeof this.min>>(values: A): A {
+    if (values.length !== this.length) {
+      throw new Error(`Values array must have length ${this.length}`)
+    }
+
     return values.map((val, i) => {
-      if (!this.min[i] || !this.scale[i]) {
+      if (this.min[i] === undefined || this.scale[i] === undefined) {
         throw new Error('Values array is too big!')
       }
       return val * this.scale[i] + this.min[i]
-    })
+    }) as A
   }
 
-  inverseTransform(values: number[]): number[] {
+  inverseTransform<A extends NumberTuple<typeof this.min>>(values: A): A {
+    if (values.length !== this.length) {
+      throw new Error(`Values array must have length ${this.length}`)
+    }
+
     return values.map((val, i) => {
-      if (!this.min[i] || !this.scale[i]) {
+      if (this.min[i] === undefined || this.scale[i] === undefined) {
         throw new Error('Values array is too big!')
       }
       return (val - this.min[i]) / this.scale[i]
-    })
+    }) as A
   }
 }
--- frontend/src/helpers/temperaturePrediction.ts
+++ frontend/src/helpers/temperaturePrediction.ts
@@ -1,3 +1,5 @@
+import { MinMaxScaler, featuresScalerParams, outputScalerParams } from './scaler'
+
 export enum Sex {
   MALE = 0,
   FEMALE = 1,
@@ -15,7 +17,7 @@ export interface EnvironmentalFeatures {
   humidity: number // %RH
 }
 
-type COEFFICIENT_ARRAY = [number, number, number, number, number, number, number, number]
+export type COEFFICIENT_ARRAY = [number, number, number, number, number, number, number, number]
 
 interface PredictionProgress {
   currentStep: number
@@ -23,7 +25,12 @@ interface PredictionProgress {
 }
 
 export class TemperaturePredictor {
+  // Scalers
+  private featuresScaler = new MinMaxScaler(featuresScalerParams)
+  private outputScaler = new MinMaxScaler(outputScalerParams)
+  // Features
   private biophysicalFeatures = undefined as undefined | BiophysicalFeatures
+  // Coefficients
   // TODO: Placeholder values - replace with values trained on whole dataset
   private treCoeffs: COEFFICIENT_ARRAY = [
     1.6261586852849347e-4, 7.3681421437795942e-4, -4.3916987857211637e-4, 4.6532701146677997e-4,
@@ -36,21 +43,35 @@ export class TemperaturePredictor {
   ]
   private mtskIntercept = 4.3563287283298391e-2
 
+  // Constructor
   constructor(biophysicalFeatures: BiophysicalFeatures) {
     this.biophysicalFeatures = biophysicalFeatures
   }
 
+  // Calculate final temperature given environmental conditions
   calculateTemperatureAtConditions(
     environmentalFeatures: EnvironmentalFeatures,
     progressCallback?: (progress: PredictionProgress) => void,
   ) {
+    // Initial simulation (air conditioned room)
+    const baselineEnvironmentalFeatures = { ambientTemp: 23, humidity: 50 }
+    const baselineSimulationTime = 120
+    const initialTre = 37
+    const initialMtsk = 32
+    // Full simulation time
+    const actualSimulationTime = 540
     // Calculate baseline temperature
-    const baselineEnvironmentalFeatures = { ambientTemp: 23, humidity: 50 } // Air con room
-    const baselineTemp = this.predict(baselineEnvironmentalFeatures, 120, 37, 32, progressCallback)
+    const baselineTemp = this.predict(
+      baselineEnvironmentalFeatures,
+      baselineSimulationTime,
+      initialTre,
+      initialMtsk,
+      progressCallback,
+    )
     // Calculate rise/fall in temperature
     const finalTemp = this.predict(
       environmentalFeatures,
-      540,
+      actualSimulationTime,
       baselineTemp.rectalTemp,
       baselineTemp.skinTemp,
       progressCallback,
@@ -58,7 +79,8 @@ export class TemperaturePredictor {
     return finalTemp
   }
 
-  predict(
+  // Predict temperature for a given
+  private predict(
     environmentalFeatures: EnvironmentalFeatures,
     durationMinutes: number,
     initialTre = 37,
@@ -72,23 +94,35 @@ export class TemperaturePredictor {
         this.biophysicalFeatures,
       )
     }
-    // Precompute static components
-    const staticTre = this.calculateStaticComponent(
+
+    // Scale input features
+    const rawFeatures = this.prepareFeatures(
       environmentalFeatures,
       this.biophysicalFeatures,
+      initialTre,
+      initialMtsk,
+    )
+    const scaledFeatures = this.featuresScaler.transform(rawFeatures)
+
+    // Precompute static components
+    const staticTre = this.calculateStaticComponent(
+      scaledFeatures,
       this.treCoeffs,
       this.treIntercept,
     )
     const staticMtsk = this.calculateStaticComponent(
-      environmentalFeatures,
-      this.biophysicalFeatures,
+      scaledFeatures,
       this.mtskCoeffs,
       this.mtskIntercept,
     )
 
     // Initialize temperatures
-    let prevTre = initialTre // Initial rectal temp
-    let prevMtsk = initialMtsk // Initial skin temp
+    let prevTre = scaledFeatures[6] // Initial rectal temp
+    let prevMtsk = scaledFeatures[7] // Initial skin temp
+
+    if (prevTre === undefined || prevMtsk === undefined) {
+      throw new Error(`Undefined prevTre or prevMtsk before calculations: ${prevTre}, ${prevMtsk}`)
+    }
 
     // Run simulation
     const totalSteps = durationMinutes
@@ -111,25 +145,63 @@ export class TemperaturePredictor {
       }
     }
 
+    if (prevTre === undefined || prevMtsk === undefined) {
+      throw new Error(`Undefined prevTre or prevMtsk after calculations: ${prevTre}, ${prevMtsk}`)
+    }
+
+    // Scale back the final output
+    const scaledOutput = [prevTre, prevMtsk] as [number, number]
+    const [rectalTemp, skinTemp] = this.outputScaler.inverseTransform(scaledOutput)
+
+    if (rectalTemp === undefined || skinTemp === undefined) {
+      throw new Error(
+        `Undefined final rectalTemp or skinTemp after calculations: ${prevTre}, ${prevMtsk}`,
+      )
+    }
+
     return {
-      rectalTemp: prevTre,
-      skinTemp: prevMtsk,
+      rectalTemp,
+      skinTemp,
     }
   }
 
+  // Prepare features in correct order for scaling
+  private prepareFeatures(
+    environmental: EnvironmentalFeatures,
+    biophysical: BiophysicalFeatures,
+    inputTre: number,
+    inputMtsk: number,
+  ): COEFFICIENT_ARRAY {
+    // IMPORTANT: Maintain the exact same feature order used during training
+    return [
+      biophysical.sex,
+      biophysical.age,
+      biophysical.heightCm,
+      biophysical.massKg,
+      environmental.ambientTemp,
+      environmental.humidity,
+      inputTre,
+      inputMtsk,
+    ]
+  }
+
+  // Calculate static component of regression based of parameters that are fixed
   private calculateStaticComponent(
-    environmentalFeatures: EnvironmentalFeatures,
-    biophysicalFeatures: BiophysicalFeatures,
+    scaledFeatures: COEFFICIENT_ARRAY,
     coefficients: COEFFICIENT_ARRAY,
     intercept: number,
   ): number {
+    if (scaledFeatures.length < 8) {
+      throw new Error()
+    }
+    // Uses pre-scaled features
     return (
-      coefficients[0] * biophysicalFeatures.sex +
-      coefficients[1] * biophysicalFeatures.age +
-      coefficients[2] * biophysicalFeatures.heightCm +
-      coefficients[3] * biophysicalFeatures.massKg +
-      coefficients[4] * environmentalFeatures.ambientTemp +
-      coefficients[5] * environmentalFeatures.humidity +
+      coefficients[0] * scaledFeatures[0] +
+      coefficients[1] * scaledFeatures[1] +
+      coefficients[2] * scaledFeatures[2] +
+      coefficients[3] * scaledFeatures[3] +
+      coefficients[4] * scaledFeatures[4] +
+      coefficients[5] * scaledFeatures[5] +
       intercept
     )
   }
